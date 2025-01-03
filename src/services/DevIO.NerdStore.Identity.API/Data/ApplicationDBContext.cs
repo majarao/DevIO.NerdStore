e2c6c53525cd8bd44fe791a5656db8f5b92cbe1c
Thiago Majarão Longo <majarao@outlook.com>
2025-01-02 21:00:56 -0300

    Configuração do Identity

---
+++ src/services/DevIO.NerdStore.Identity.API/Data/ApplicationDBContext.cs
@@ -0,0 +1,6 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevIO.NerdStore.Identity.API.Data;
+
+public class ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : IdentityDbContext(options) { }
--- src/services/DevIO.NerdStore.Identity.API/Program.cs
+++ src/services/DevIO.NerdStore.Identity.API/Program.cs
@@ -1,4 +1,16 @@
+using DevIO.NerdStore.Identity.API.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ApplicationDBContext>(
+    options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>()
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDBContext>()
+    .AddDefaultTokenProviders();
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
@@ -14,6 +26,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+app.UseAuthentication();
 
 app.MapControllers();
 
