65c2b43d2141f6612165f55becad37f194d2ce82
Thiago Majarão Longo <majarao@outlook.com>
2025-01-02 21:52:35 -0300

    Configurando o JWT na API

--- src/services/DevIO.NerdStore.Identity.API/Program.cs
+++ src/services/DevIO.NerdStore.Identity.API/Program.cs
@@ -1,16 +1,49 @@
 using DevIO.NerdStore.Identity.API.Data;
+using DevIO.NerdStore.Identity.API.Extensions;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<ApplicationDBContext>(
     options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddDefaultIdentity<IdentityUser>()
+builder.Services
+    .AddDefaultIdentity<IdentityUser>()
     .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDBContext>()
     .AddDefaultTokenProviders();
+
+IConfigurationSection appSettingsSection = builder.Configuration.GetSection("AppSettings");
+builder.Services.Configure<AppSettings>(appSettingsSection);
+
+AppSettings? appSettings = appSettingsSection.Get<AppSettings>();
+byte[] key = Encoding.ASCII.GetBytes(appSettings?.Secret ?? "x");
+
+builder.Services
+    .AddAuthentication(options => 
+    {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(bearerOptions =>
+    {
+        bearerOptions.RequireHttpsMetadata = true;
+        bearerOptions.SaveToken = true;
+        bearerOptions.TokenValidationParameters =
+            new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidAudience = appSettings?.ValidoEm ?? "x",
+                ValidIssuer = appSettings?.Emissor ?? "x"
+            };
+    });
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
@@ -18,7 +51,8 @@ builder.Services.AddOpenApi();
 WebApplication app = builder.Build();
 
 app.MapOpenApi();
-app.UseSwaggerUI(options => options.SwaggerEndpoint("/openapi/v1.json", "Identity API"));
+app.UseSwaggerUI(
+    options => options.SwaggerEndpoint("/openapi/v1.json", "Identity API"));
 
 app.UseHttpsRedirection();
 
