294386562600ddd89f6d5728302821c1b147a7b0
Thiago Majarão Longo <majarao@outlook.com>
2025-01-07 21:41:46 -0300

    Layout do e-commerce

---
+++ src/web/DevIO.NerdStore.WebApp.MVC/Extensions/RazorHelpers.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using Microsoft.AspNetCore.Mvc.Razor;
+
+namespace DevIO.NerdStore.WebApp.MVC.Extensions;
+
+public static class RazorHelpers
+{
+    public static string HashEmailForGravatar(this RazorPage page, string email)
+    {
+        byte[] data = MD5.HashData(Encoding.Default.GetBytes(email));
+        StringBuilder sBuilder = new();
+        foreach (byte t in data)
+            sBuilder.Append(t.ToString("x2"));
+
+        return sBuilder.ToString();
+    }
+
+    public static string FormatoMoeda(this RazorPage page, decimal valor) =>
+        valor > 0 ? string.Format(Thread.CurrentThread.CurrentCulture, "{0:C}", valor) : "Gratuito";
+
+    public static string MensagemEstoque(this RazorPage page, int quantidade) =>
+        quantidade > 0 ? $"Apenas {quantidade} em estoque!" : "Produto esgotado!";
+}
--- src/web/DevIO.NerdStore.WebApp.MVC/Models/ErrorViewModel.cs
+++ src/web/DevIO.NerdStore.WebApp.MVC/Models/ErrorViewModel.cs
@@ -1,21 +1,20 @@
-namespace DevIO.NerdStore.WebApp.MVC.Models
+namespace DevIO.NerdStore.WebApp.MVC.Models;
+
+public class ErrorViewModel
 {
-    public class ErrorViewModel
-    {
-        public int ErroCode { get; set; }
-        public string Titulo { get; set; } = string.Empty;
-        public string Mensagem { get; set; } = string.Empty;
-    }
+    public int ErroCode { get; set; }
+    public string Titulo { get; set; } = string.Empty;
+    public string Mensagem { get; set; } = string.Empty;
+}
 
-    public class ResponseResult
-    {
-        public string Title { get; set; } = string.Empty;
-        public int Status { get; set; }
-        public ResponseErrorMessages? Errors { get; set; }
-    }
+public class ResponseResult
+{
+    public string Title { get; set; } = string.Empty;
+    public int Status { get; set; }
+    public ResponseErrorMessages? Errors { get; set; }
+}
 
-    public class ResponseErrorMessages
-    {
-        public List<string> Mensagens { get; set; } = [];
-    }
+public class ResponseErrorMessages
+{
+    public List<string> Mensagens { get; set; } = [];
 }
