f9a3ec82da19fc226604a73644170712b09fd7ba
Thiago Majarão Longo <majarao@outlook.com>
2025-01-07 21:59:55 -0300

    Modelagem API Catalogo

---
+++ src/building blocks/DevIO.NerdStore.Core/DomainObjects/Entity.cs
@@ -0,0 +1,36 @@
+﻿namespace DevIO.NerdStore.Core.DomainObjects;
+
+public abstract class Entity
+{
+    public Guid Id { get; set; }
+
+    public override bool Equals(object? obj)
+    {
+        Entity? compareTo = obj as Entity;
+
+        if (ReferenceEquals(this, compareTo))
+            return true;
+
+        if (compareTo is null)
+            return false;
+
+        return Id.Equals(compareTo.Id);
+    }
+
+    public override int GetHashCode() => GetType().GetHashCode() * 907 + Id.GetHashCode();
+
+    public override string ToString() => $"{GetType().Name} [Id={Id}]";
+
+    public static bool operator ==(Entity a, Entity b)
+    {
+        if (a is null && b is null)
+            return true;
+
+        if (a is null || b is null)
+            return false;
+
+        return a.Equals(b);
+    }
+
+    public static bool operator !=(Entity a, Entity b) => !(a == b);
+}
