41dfd82fe00ba118dd47ac54923ab7b3f34f0942
Thiago Majarão Longo <majarao@outlook.com>
2025-01-13 21:16:06 -0300

    Validação de acesso baseado em claims

---
+++ src/building blocks/DevIO.NerdStore.WebAPI.Core/Identity/CustomAuthorize.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Security.Claims;
+
+namespace DevIO.NerdStore.WebAPI.Core.Identity;
+
+public class CustomAuthorization
+{
+    public static bool ValidarClaimsUsuario(HttpContext? context, string claimName, string claimValue) =>
+        (context?.User?.Identity?.IsAuthenticated ?? false) && context.User.Claims.Any(c => c.Type == claimName && c.Value.Contains(claimValue));
+}
+
+public class ClaimsAuthorizeAttribute : TypeFilterAttribute
+{
+    public ClaimsAuthorizeAttribute(string claimName, string claimValue) : base(typeof(RequisitoClaimFilter))
+    {
+        Arguments = [claimName, claimValue];
+    }
+}
+
+public class RequisitoClaimFilter(Claim claim) : IAuthorizationFilter
+{
+    private Claim Claim { get; } = claim;
+
+    public void OnAuthorization(AuthorizationFilterContext context)
+    {
+        if (!context.HttpContext?.User?.Identity?.IsAuthenticated ?? false)
+        {
+            context.Result = new StatusCodeResult(401);
+            return;
+        }
+
+        if (!CustomAuthorization.ValidarClaimsUsuario(context.HttpContext, Claim.Type, Claim.Value))
+            context.Result = new StatusCodeResult(403);
+    }
+}
--- src/services/DevIO.NerdStore.Catalogo.API/Controllers/CatalogoController.cs
+++ src/services/DevIO.NerdStore.Catalogo.API/Controllers/CatalogoController.cs
@@ -1,4 +1,5 @@
 ﻿using DevIO.NerdStore.Catalogo.API.Models;
+using DevIO.NerdStore.WebAPI.Core.Identity;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,10 +11,12 @@ public class CatalogoController(IProdutoRepository repository) : Controller
 {
     private IProdutoRepository Repository { get; } = repository;
 
+    [AllowAnonymous]
     [HttpGet("catalogo/produtos")]
     public async Task<IEnumerable<Produto>> Index() =>
         await Repository.ObterTodos();
 
+    [ClaimsAuthorize("Catalogo", "Ler")]
     [HttpGet("catalogo/produtos/{id}")]
     public async Task<Produto?> ProdutoDetalhe(Guid id) =>
         await Repository.ObterPorId(id);
