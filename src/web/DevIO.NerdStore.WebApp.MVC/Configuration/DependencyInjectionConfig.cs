479ccfffebfe790a8b43eede7d05a132963aa27a
Thiago Majarão Longo <majarao@outlook.com>
2025-01-15 21:26:43 -0300

    Retry Pattern com Polly

--- src/web/DevIO.NerdStore.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
+++ src/web/DevIO.NerdStore.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
@@ -1,6 +1,9 @@
 ﻿using DevIO.NerdStore.WebApp.MVC.Extensions;
 using DevIO.NerdStore.WebApp.MVC.Services;
 using DevIO.NerdStore.WebApp.MVC.Services.Handlers;
+using Polly;
+using Polly.Extensions.Http;
+using Polly.Retry;
 
 namespace DevIO.NerdStore.WebApp.MVC.Configuration;
 
@@ -12,14 +15,30 @@ public static class DependencyInjectionConfig
 
         services.AddHttpClient<IAutenticacaoService, AutenticacaoService>();
 
+        AsyncRetryPolicy<HttpResponseMessage> retryWaitPolicy = HttpPolicyExtensions
+            .HandleTransientHttpError()
+            .WaitAndRetryAsync(
+            [
+                TimeSpan.FromSeconds(1),
+                TimeSpan.FromSeconds(5),
+                TimeSpan.FromSeconds(10)
+            ], (outcome, timespan, retryCount, context) =>
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine($"Tentando pela {retryCount} vez!");
+                Console.ForegroundColor = ConsoleColor.White;
+            });
+
         services.AddHttpClient("Refit", options =>
         {
             options.BaseAddress = new Uri(configuration.GetSection("CatalogoUrl").Value ?? string.Empty);
         })
             .AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>()
-            .AddTypedClient(Refit.RestService.For<ICatalogoService>);
+            .AddTypedClient(Refit.RestService.For<ICatalogoService>)
+            .AddPolicyHandler(retryWaitPolicy);
 
         services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
         services.AddScoped<IUser, AspNetUser>();
 
         return services;
