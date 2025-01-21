1b56d9949c4b0a4762beb52426b1557af719622b
Thiago Majarão Longo <majarao@outlook.com>
2025-01-20 21:05:40 -0300

    Definindo as entidades

---
+++ src/services/DevIO.NerdStore.Cliente.API/Models/Endereco.cs
@@ -0,0 +1,16 @@
+﻿using DevIO.NerdStore.Core.DomainObjects;
+
+namespace DevIO.NerdStore.Cliente.API.Models;
+
+public class Endereco(string logradouro, string numero, string complemento, string bairro, string cep, string cidade, string estado) : Entity
+{
+    public string Logradouro { get; private set; } = logradouro;
+    public string Numero { get; private set; } = numero;
+    public string Complemento { get; private set; } = complemento;
+    public string Bairro { get; private set; } = bairro;
+    public string Cep { get; private set; } = cep;
+    public string Cidade { get; private set; } = cidade;
+    public string Estado { get; private set; } = estado;
+    public Guid ClienteId { get; private set; }
+    public Cliente? Cliente { get; protected set; }
+}
