b4dae371fe6ac60afb20388bc2def68cd58e8d39
Thiago Majarão Longo <majarao@outlook.com>
2025-02-17 21:12:58 -0300

    Modelagem do pedido no domínio

---
+++ src/services/DevIO.NerdStore.Pedido.Domain/Pedidos/Pedido.cs
@@ -0,0 +1,85 @@
+﻿using DevIO.NerdStore.Pedido.Domain.Vouchers;
+
+namespace DevIO.NerdStore.Pedido.Domain.Pedidos;
+
+public class Pedido
+{
+    public int Codigo { get; private set; }
+    public Guid ClienteId { get; private set; }
+    public Guid? VoucherId { get; private set; }
+    public bool VoucherUtilizado { get; private set; }
+    public decimal Desconto { get; private set; }
+    public decimal ValorTotal { get; private set; }
+    public DateTime DataCadastro { get; private set; }
+    public PedidoStatus PedidoStatus { get; private set; }
+
+    private readonly List<PedidoItem> _pedidoItems = [];
+    public IReadOnlyCollection<PedidoItem> PedidoItems => _pedidoItems;
+
+    public Endereco? Endereco { get; private set; }
+    public Voucher? Voucher { get; private set; }
+
+    protected Pedido() { }
+
+    public Pedido(
+        Guid clienteId,
+        decimal valorTotal,
+        List<PedidoItem> pedidoItems,
+        bool voucherUtilizado = false,
+        decimal desconto = 0,
+        Guid? voucherId = null)
+    {
+        ClienteId = clienteId;
+        ValorTotal = valorTotal;
+        _pedidoItems = pedidoItems;
+        Desconto = desconto;
+        VoucherUtilizado = voucherUtilizado;
+        VoucherId = voucherId;
+    }
+
+    public void AutorizarPedido() => PedidoStatus = PedidoStatus.Autorizado;
+
+    public void AtribuirVoucher(Voucher voucher)
+    {
+        VoucherUtilizado = true;
+        VoucherId = voucher.Id;
+        Voucher = voucher;
+    }
+
+    public void AtribuirEndereco(Endereco endereco) => Endereco = endereco;
+
+    public void CalcularValorPedido()
+    {
+        ValorTotal = PedidoItems.Sum(p => p.CalcularValor());
+        CalcularValorTotalDesconto();
+    }
+
+    public void CalcularValorTotalDesconto()
+    {
+        if (!VoucherUtilizado) 
+            return;
+
+        decimal desconto = 0;
+        decimal valor = ValorTotal;
+
+        if (Voucher?.TipoDesconto == TipoDescontoVoucher.Porcentagem)
+        {
+            if (Voucher.Percentual > 0)
+            {
+                desconto = (valor * Voucher.Percentual) / 100;
+                valor -= desconto;
+            }
+        }
+        else
+        {
+            if (Voucher?.ValorDesconto > 0)
+            {
+                desconto = Voucher.ValorDesconto;
+                valor -= desconto;
+            }
+        }
+
+        ValorTotal = valor < 0 ? 0 : valor;
+        Desconto = desconto;
+    }
+}
